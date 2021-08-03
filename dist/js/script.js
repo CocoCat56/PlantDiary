0e18fe770c48772131201cb128a00cf82ae9939c
CocoCat56 <zo7758002@gmail.com>
2021-08-03 15:41:13 +0800

    更新 script.js
    加入註解

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,6 +7,7 @@ let downIcon = document.querySelector(".downIcon");
 var diaryList = [];
 createItem.addEventListener("click", e => {
     e.preventDefault();
+    //取得使用者輸入的資料
     let items = document.querySelector(".items");
     let title = items.children[0].children[1];
     let addDate = items.children[1].children[1];
@@ -15,7 +16,7 @@ createItem.addEventListener("click", e => {
     let date = addDate.value.substring(8);
     let picture = items.children[2].children[1];
     let desc = items.children[3].children[1];
-    
+    //未輸入的提示訊息
     if(title.value === ""){
         alert("請填寫標題");
         title.parentElement.children[1].select();
@@ -40,6 +41,7 @@ createItem.addEventListener("click", e => {
     delItem.addEventListener("click", e => {
         let delitem = e.target.parentElement;
         let deltext = delitem.children[2].innerText;
+        //從陣列 ( diaryList )中刪除
         diaryList.forEach((item, index) => {
             if(item.title == deltext) {
                 diaryList.splice(index, 1);
@@ -50,16 +52,17 @@ createItem.addEventListener("click", e => {
         }
         delitem.remove();
     });
+    //h2標題
     let heading2 = document.createElement("h2");
     heading2.innerText = title.value;
-
+    //日期
     let diaryDate = document.createElement("p");
     diaryDate.classList.add("diaryDate");
     diaryDate.innerHTML ="<i class='far fa-calendar-check'></i>" +  year + "/" + month + "/" + date;
-    
+    //照片
     let image = document.createElement("img");
     image.src = picture.value;
-
+    //描述
     let diaryDesc = document.createElement("p");
     diaryDesc.classList.add("diaryDesc");
     diaryDesc.innerText = desc.value;
@@ -70,12 +73,7 @@ createItem.addEventListener("click", e => {
     diaryItem.appendChild(diaryDesc);
     diaryItem.appendChild(diaryDate);
 
-    if(diaryList.length !== 0) {
-        downIcon.style.visibility = "";
-    } else {
-        downIcon.style.visibility = "hidden";
-    }
-    //建立diary object
+    //建立 diary object，並加到陣列
     let diary = {
         title: title.value,
         year: year,
@@ -84,8 +82,14 @@ createItem.addEventListener("click", e => {
         image: picture.value,
         desc: desc.value
     };
-    
     diaryList.push(diary);
+    //是否顯示 icon
+    if(diaryList.length !== 0) {
+        downIcon.style.visibility = "";
+    } else {
+        downIcon.style.visibility = "hidden";
+    }
+
     //清空欄位
     title.value = ""; 
     addDate.value = "";
@@ -108,7 +112,6 @@ function loadData() {
     };
     diaryList.push(record1);
     if(diaryList.length !== 0) {
-        
         for(let i in diaryList) {
             let diaryItem = document.createElement("div");
             diaryItem.classList.add("diaryItem");
@@ -128,11 +131,10 @@ function loadData() {
                 }
                 delitem.remove();
             });
-            
-            
-            let img = document.createElement("img");
-            img.src = diaryList[i].image;
-
+            //照片
+            let image = document.createElement("img");
+            image.src = diaryList[i].image;
+            //h2
             let heading2 = document.createElement("h2");
             heading2.innerText = diaryList[i].title;
             
@@ -145,7 +147,7 @@ function loadData() {
             diaryDate.innerHTML = "<i class='far fa-calendar-check'></i>" + diaryList[i].year + "/" + diaryList[i].month + "/" + diaryList[i].date;
 
             diaryItem.appendChild(delItem);
-            diaryItem.appendChild(img);
+            diaryItem.appendChild(image);
             diaryItem.appendChild(heading2);
             diaryItem.appendChild(diaryDesc);
             diaryItem.appendChild(diaryDate);
